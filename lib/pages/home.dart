fe4995a2028d31b54c09e34133744348b6729a52
merve <merve@MERVEA>
2024-07-08 21:02:28 +0300

    Commit

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatelessWidget {
@@ -6,62 +7,165 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return appBar();
+    return Scaffold(
+      appBar: appBar(),
+      backgroundColor: Colors.white,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          _searchField(),
+          SizedBox(
+            height: 40,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(left: 20),
+                child: Text(
+                  'Popular',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Container(
+                height: 150,
+                color: Colors.green,
+                child: ListView.builder(
+                  itemCount: 2,
+                  itemBuilder: (context, index) {
+                    if (index == 0) {
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text('Salad'),
+                      );
+                    } else if (index == 1) {
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text('Cake'),
+                      );
+                    }
+                    return Container();
+                  },
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 
-  Scaffold appBar() {
-    return Scaffold(
-    appBar: AppBar(
-      title: Text(
-        'Breakfast',
-      style: TextStyle(
-        color: Colors.black,
-        fontSize: 18,
-        fontWeight: FontWeight.bold
+  Widget _searchField() {
+    return Container(
+      margin: EdgeInsets.only(top: 40, left: 20, right: 20),
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Color(0xff1D1617).withOpacity(0.11),
+            spreadRadius: 0.0,
+            blurRadius: 40,
+          ),
+        ],
       ),
+      child: TextField(
+        decoration: InputDecoration(
+          filled: true,
+          fillColor: Colors.white,
+          contentPadding: EdgeInsets.all(15),
+          hintText: 'Search Pancake',
+          hintStyle: TextStyle(
+            color: Color(0xffDDDADA),
+            fontSize: 14,
+          ),
+          prefixIcon: Padding(
+            padding: const EdgeInsets.all(12),
+            child: SvgPicture.asset('assets/icons/search.svg'),
+          ),
+          suffixIcon: Container(
+            width: 100,
+            child: IntrinsicHeight(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end, // dikey çizgi
+                children: [
+                  VerticalDivider(
+                    // dikey çizgi özellikleri
+                    color: Colors.black,
+                    indent: 10,
+                    endIndent: 10,
+                    thickness: 0.1,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SvgPicture.asset('assets/icons/Filter.svg'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(15),
+            borderSide: BorderSide.none,
+          ),
+        ),
+      ),
+    );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      title: const Text(
+        'Breakfast',
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+        ),
       ),
       backgroundColor: Colors.white,
       elevation: 0.0,
       centerTitle: true,
       leading: GestureDetector(
-        onTap:(){
-          
-        },
+        onTap: () {},
         child: Container(
-                    margin: EdgeInsets.all(10),
-        alignment: Alignment.center,
-        child: SvgPicture.asset('assets/icons/Arrow - Left 2.svg',
-        height: 20,
-        width: 20,),
-        decoration: BoxDecoration(
-          color: Color(0xffF7F8F8),
-          borderRadius: BorderRadius.circular(10),
+          margin: const EdgeInsets.all(10),
+          alignment: Alignment.center,
+          child: SvgPicture.asset(
+            'assets/icons/Arrow - Left 2.svg',
+            height: 20,
+            width: 20,
+          ),
+          decoration: BoxDecoration(
+            color: const Color(0xffF7F8F8),
+            borderRadius: BorderRadius.circular(10),
+          ),
         ),
       ),
-
-        ),
-
       actions: [
         GestureDetector(
-          onTap: (){
-            
-          },
-          child:Container(
-        margin: EdgeInsets.all(10),
-        alignment: Alignment.center,
-        width: 37,
-        child: SvgPicture.asset('assets/icons/dots.svg',
-        height: 5,
-        width: 5,),
-
-        decoration: BoxDecoration(
-          color: Color(0xffF7F8F8),
-          borderRadius: BorderRadius.circular(10),
+          onTap: () {},
+          child: Container(
+            margin: const EdgeInsets.all(10),
+            alignment: Alignment.center,
+            width: 37,
+            child: SvgPicture.asset(
+              'assets/icons/dots.svg',
+              height: 5,
+              width: 5,
+            ),
+            decoration: BoxDecoration(
+              color: const Color(0xffF7F8F8),
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
         ),
-        ),
-      ),
       ],
-    ),
-  );
+    );
   }
 }
